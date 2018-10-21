06b1966185fd0d3baea1eb4990666386e94fa22e
EC2 Default User <ec2-user@ip-10-4-1-86.ap-northeast-1.compute.internal>
2018-10-21 13:44:30 +0900

    up

--- index.js
+++ index.js
@@ -4,20 +4,22 @@ const fs = require('fs');
 const CACHE_DIR = '/tmp/fonts-cache';
 
 exports.handler = (event, context, callback) => {
+    process.env['FONTCONFIG_PATH'] = '/var/task/fonts';
+
     console.log("process.cwd =", process.cwd());
     fs.mkdirSync(CACHE_DIR);
 
     exec('fc-cache -v /var/task/fonts', function(err, stdout, stderr) {
         if (err) {
             console.error(err);
-            context.fail(err);
+            context.fail("NG");
             return;
         }
 
         exec('ls -la /tmp', function(err, stdout, stderr) {
             if (err) {
                 console.error(err);
-                context.fail(err);
+                context.fail("NG");
                 return;
             }
             console.log(stdout);
@@ -25,4 +27,3 @@ exports.handler = (event, context, callback) => {
         });
     });
 };
-
